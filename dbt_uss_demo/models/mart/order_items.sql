bd1cdb94035c34178f477ff5e940b0fad7f79f67
linh nguyen <nguyen.hoang.linh@wolt.com>
2025-06-17 14:51:57 +0200

    small fixes on order_items order_payments

--- dbt_uss_demo/models/mart/order_items.sql
+++ dbt_uss_demo/models/mart/order_items.sql
@@ -1,8 +1,13 @@
 select
-    _key_order_item_id as order_item_id,
     shipping_limit_date,
     price,
     freight_value,
+    md5(
+        concat(
+            cast(order_id as string),
+            cast(order_item_id as string)
+        )
+    ) as order_item_id,
     md5(
         concat(
             cast(order_id as string),
--- dbt_uss_demo/models/mart/order_payments.sql
+++ dbt_uss_demo/models/mart/order_payments.sql
@@ -1,9 +1,14 @@
 select
-    _key_payment_id as payment_id,
     payment_sequential
         as payment_type,
     payment_installments,
     payment_value,
+    md5(
+        concat(
+            order_id::text,
+            payment_sequential::text
+        )
+    ) as payment_id,
     md5(
         concat(
             order_id::text,
